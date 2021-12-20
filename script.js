d07c63d04977a04533ac4a4441cdb594c4f3667e
naila2311 <94777483+naila2311@users.noreply.github.com>
2021-12-20 21:48:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -118,3 +118,25 @@ function init() {
 init();
 
 form.addEventListener('submit', addTransaction);
+<script type="text/script.js">
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  
+  ['grocery, 80],
+  ['salary', 1000],
+  ['Rent', 200],
+  ['Bills', 200],
+  ]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'My Average Budget', 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
